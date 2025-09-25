7940dbe18cd71bc6236e766b450ac57742a4a62e
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-09-25 12:46:14 -0700

    Adds return to summary and add_class functions

--- activity/comparison.py
+++ activity/comparison.py
@@ -3,4 +3,4 @@ def get_student_with_more_classes(student_1, student_2):
     if student_1.get_num_classes() > student_2.get_num_classes():
         return student_1
     else:
-        return student_2 
+        return student_2
--- activity/student.py
+++ activity/student.py
@@ -6,12 +6,14 @@ class Student:
     
     def add_class(self, course):
         self.courses.append(course)
+        return self.courses
 
     def get_num_classes(self):
         return len(self.courses)
 
-    
     def summary(self):
-        print(f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes")
+        return f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes"
+
+        
 
 
